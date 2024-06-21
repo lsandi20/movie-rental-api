51e4bb6ba3f4a0010d52b97e4bfbb04c750ff0a8
lfs <luthfisandi20@gmail.com>
2024-06-22 06:41:02 +0700

    feat: add validation logic for rent movie

--- src/rentals/rentals.service.ts
+++ src/rentals/rentals.service.ts
@@ -10,15 +10,20 @@ import { Rental } from './entities/rental.entity';
 import { v4 as uuidv4 } from 'uuid';
 import { Movie } from 'src/movies/entities/movie.entity';
 import { User } from 'src/users/entities/user.entity';
+import { Op } from 'sequelize';
 
 @Injectable()
 export class RentalsService {
   constructor(
     @InjectModel(Rental)
     private rentalModel: typeof Rental,
+    @InjectModel(Movie)
+    private movieModel: typeof Movie,
+    @InjectModel(User)
+    private userModel: typeof User,
   ) {}
 
-  create(createRentalDto: CreateRentalDto) {
+  async create(createRentalDto: CreateRentalDto) {
     const { movie_id, user_id, rental_start, rental_end } = createRentalDto;
     const newRental = {
       id: uuidv4(),
@@ -28,13 +33,43 @@ export class RentalsService {
       rental_end,
     };
 
-    if (rental_start > rental_end) {
+    const movie = await this.movieModel.findOne({ where: { id: movie_id } });
+    if (!movie) {
+      throw new NotFoundException('movie not found');
+    }
+
+    const user = await this.userModel.findOne({ where: { id: user_id } });
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+
+    if (user.role !== 'customer') {
+      throw new BadRequestException('user must be customer');
+    }
+
+    // validate currently rented movie
+    const currentRent = await this.rentalModel.findOne({
+      where: {
+        movie_id,
+        rental_end: {
+          [Op.gte]: rental_start,
+        },
+      },
+    });
+
+    if (currentRent) {
+      throw new BadRequestException('movie currently rented');
+    }
+
+    if (rental_start >= rental_end) {
       throw new BadRequestException(
-        'rental start date should not ahead of rental end date',
+        'rental start date time should not ahead of rental end date time',
       );
     }
 
-    return this.rentalModel.create(newRental);
+    await this.rentalModel.create(newRental);
+
+    return this.findOne(newRental.id);
   }
 
   findAll() {
